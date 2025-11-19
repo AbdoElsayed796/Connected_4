c843591fbff2c08ba168d0e8eb44ffaa63ac6601
Ahmed Mansoure <amansoure11@gmail.com>
2025-11-19 22:37:21 +0200

    Fixed Expectiminimax

--- Expectiminimax.py
+++ Expectiminimax.py
@@ -1,11 +1,16 @@
 import math
+from Heuristic import heuristic
 
-def Expectimimimax(current_board, max_depth=5):
+
+def Expectiminimax(current_board, max_depth=5):
+    """AI is player 2 (maximizing player)"""
     value, move = Max_Value(current_board, max_depth)
     return move
 
+
 def Max_Value(current_board, max_depth):
-    if is_full(current_board) or max_depth == 0:
+    """Player 2 (AI) maximizes"""
+    if is_full(current_board) or max_depth <= 0:
         return heuristic(current_board), None
     
     value = float('-inf')
@@ -14,10 +19,8 @@ def Max_Value(current_board, max_depth):
     for col in range(7):
         row = find_lowest_empty_row(current_board, col)
         
-        if row != -1:  
-            new_board = copy_board(current_board)
-            new_board[row][col] = 2     
-            v2, _ = Expected_Value(new_board, max_depth - 1, 1)
+        if row != -1:
+            v2 = Expected_Value(current_board, max_depth - 1, 2, col)
             
             if v2 > value:
                 value = v2
@@ -25,8 +28,10 @@ def Max_Value(current_board, max_depth):
     
     return value, best_move
 
+
 def Min_Value(current_board, max_depth):
-    if is_full(current_board) or max_depth == 0:
+    """Player 1 (opponent) minimizes"""
+    if is_full(current_board) or max_depth <= 0:
         return heuristic(current_board), None
     
     value = float('inf')
@@ -35,68 +40,78 @@ def Min_Value(current_board, max_depth):
     for col in range(7):
         row = find_lowest_empty_row(current_board, col)
         
-        if row != -1: 
-            new_board = copy_board(current_board)
-            new_board[row][col] = 1
-            v2, _ = Expected_Value(new_board, max_depth - 1, 2)
+        if row != -1:
+            v2 = Expected_Value(current_board, max_depth - 1, 1, col)
+            
             if v2 < value:
                 value = v2
                 best_move = col
     
     return value, best_move
 
-def Expected_Value(current_board, max_depth, player):
-    if is_full(current_board) or max_depth == 0 :
-        return heuristic(current_board), None
 
-    value = 0
-    best_move = None
+def Expected_Value(current_board, max_depth, player, col):
+    """Calculate expected value after a stochastic move"""
+    row = find_lowest_empty_row(current_board, col)
+    
+    if row == -1:
+        return float('-inf') if player == 2 else float('inf')
+    
+    can_go_left = (col > 0) and (find_lowest_empty_row(current_board, col - 1) != -1)
+    can_go_right = (col < 6) and (find_lowest_empty_row(current_board, col + 1) != -1)
+    
+    if can_go_left and can_go_right:
+        prob_center, prob_left, prob_right = 0.6, 0.2, 0.2
+    elif can_go_left:
+        prob_center, prob_left, prob_right = 0.8, 0.2, 0.0
+    elif can_go_right:
+        prob_center, prob_left, prob_right = 0.8, 0.0, 0.2
+    else:
+        prob_center, prob_left, prob_right = 1.0, 0.0, 0.0
+    
+    value = 0.0
+    
+    new_board = copy_board(current_board)
+    new_board[row][col] = player
+    if player == 2:
+        value += prob_center * Min_Value(new_board, max_depth - 1)[0]
+    else:
+        value += prob_center * Max_Value(new_board, max_depth - 1)[0]
+    
+    if can_go_left:
+        row_left = find_lowest_empty_row(current_board, col - 1)
+        new_board_left = copy_board(current_board)
+        new_board_left[row_left][col - 1] = player
+        if player == 2:
+            value += prob_left * Min_Value(new_board_left, max_depth - 1)[0]
+        else:
+            value += prob_left * Max_Value(new_board_left, max_depth - 1)[0]
+    
+    if can_go_right:
+        row_right = find_lowest_empty_row(current_board, col + 1)
+        new_board_right = copy_board(current_board)
+        new_board_right[row_right][col + 1] = player
+        if player == 2:
+            value += prob_right * Min_Value(new_board_right, max_depth - 1)[0]
+        else:
+            value += prob_right * Max_Value(new_board_right, max_depth - 1)[0]
+    
+    return value
 
-    for col in range(7):
-         row = find_lowest_empty_row(current_board, col)
-         if row != -1:
-            new_boar_boardd = copy(curren
-            new_board[row][col] = t_board)player
-            new_false_left_board = copy_board(current_board)
-            new_false_right_board = copy_board(current_board)
-            if col != 0:
-                new_false_left_board[row][col - 1] = player
-            if col != 6:
-                new_false_right_board[row][col + 1] = player
-            if col == 0 and player == 2:
-                value = (0.6*Max_Value(new_board,max_depth-1)+0.4*Max_Value(new_false_right_board,max_depth-1))
-            elif col == 6 and player == 2:
-                value = (0.6*Max_Value(new_board,max_depth-1)+0.4*Max_Value(new_false_left_board,max_depth-1))
-            elif player == 2:
-                value = (0.6*Max_Value(new_board,max_depth-1)+0.2*Max_Value(new_false_left_board,max_depth-1)
-                +0.2*Max_Value(new_false_right_board,max_depth-1))
-            elif col == 0 and player == 1:
-                value = (0.6*Min_Value(new_board,max_depth-1)+0.4*Min_Value(new_false_right_board,max_depth-1))
-            elif col == 6 and player == 1:
-                value = (0.6*Min_Value(new_board,max_depth-1)+0.4*Min_Value(new_false_left_board,max_depth-1))
-            else:
-                value = (0.6*Min_Value(new_board,max_depth-1)+0.2*Min_Value(new_false_left_board,max_depth-1)
-                +0.2*Min_Value(new_false_right_board,max_depth-1))
 
-        return value, None
-
-               
-
-def find_lowest_empty_row(board,r col):
+def find_lowest_empty_row(board, col):
     for row in range(5, -1, -1):
         if board[row][col] == 0:
             return row
     return -1
 
+
 def copy_board(board):
     return [row[:] for row in board]
+
 
 def is_full(board):
     for col in range(7):
         if board[0][col] == 0:
             return False
     return True
-         
-def heuristic(currnet_board):           
-    pass 
-    
